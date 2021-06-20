af95423aa7d777e2611d288812ae7487015a53a6
Temirlan Dzhoroev <dzhoroev1@gmail.com>
2021-06-20 12:40:14 +0900

    06.20

--- three_slam.py
+++ three_slam.py
@@ -15,21 +15,24 @@ class simple_motion:
 		self.arm_pub = rospy.Publisher('move_arm', String, queue_size=1)
 		self.status_sub = rospy.Subscriber("status", String, self.bring_home)
 		self.stop_pub = rospy.Publisher('reset_yolo', Int32, queue_size=1)
-		self.stop = 0
+		self.bottle_sub = rospy.Subscriber('bottle_visible', Bool, self.bottle_visible)
+		self.bottle_visible = False
 		self.bottle_collected = 0
 		rospy.sleep(1)
 		self.rotate(90)
 
+	def bottle_visible(self, bottle_visible):
+		self.bottle_visible = bottle_visible.data
+
 	def bring_home(self, s):
 		self.arm_pub.publish("2")
-		self.stop += 1
 		rospy.sleep(15)
 		goal = MoveBaseGoal()
 		##Bringin back
 		goal.target_pose.header.frame_id = "map"
 		goal.target_pose.header.stamp = rospy.Time.now()
-		goal.target_pose.pose.position.x = 0.3
-		goal.target_pose.pose.position.y = -0.05
+		goal.target_pose.pose.position.x = 0.2
+		goal.target_pose.pose.position.y = -0.2
 		goal.target_pose.pose.position.z = 0
 		goal.target_pose.pose.orientation.x = 0
 		goal.target_pose.pose.orientation.y = 0
@@ -45,7 +48,23 @@ class simple_motion:
 				self.rotate(180)
 				#rospy.loginfo("publishing 0 back")
 				self.stop_pub.publish(0)
+				self.stop = 0
 				self.rotate(90)
+				if not self.bottle_visible and self.bottle_collected < 2:
+					##move to center					
+					goal = MoveBaseGoal()
+					goal.target_pose.header.frame_id = "map"
+					goal.target_pose.header.stamp = rospy.Time.now()
+					goal.target_pose.pose.position.x = 1.9
+					goal.target_pose.pose.position.y = -0.2
+					goal.target_pose.pose.position.z = 0
+					goal.target_pose.pose.orientation.x = 0
+					goal.target_pose.pose.orientation.y = 0
+					goal.target_pose.pose.orientation.z = 0
+					goal.target_pose.pose.orientation.w = 0
+					res = self.move_to_position(goal)
+					if res:
+						rotate(270)
 
 
 	def move_to_position(self, goal):
@@ -59,24 +78,6 @@ class simple_motion:
     		else:
         		return True
 
-	def move(self, dest):
-		goal = MoveBaseGoal()
-		if dest == "h":##Bringin back
-			rospy.sleep(15)
-			goal.target_pose.header.frame_id = "map"
-			goal.target_pose.header.stamp = rospy.Time.now()
-			goal.target_pose.pose.position.x = 0.3
-			goal.target_pose.pose.position.y = -0.05
-			goal.target_pose.pose.position.z = 0
-			goal.target_pose.pose.orientation.x = 0
-			goal.target_pose.pose.orientation.y = 0
-			goal.target_pose.pose.orientation.z = 1
-			goal.target_pose.pose.orientation.w = 0
-			res = self.move_to_position(goal)
-			if res:
-				self.arm_pub.publish("d")
-				rospy.sleep(15)
-
 	def rotate(self, angle):
 		twist = Twist()
 		twist.angular.x = 0
@@ -89,15 +90,24 @@ class simple_motion:
 			self.pub.publish(twist)
 			rospy.sleep(3.5)
 
-			rospy.loginfo(twist)
+			#rospy.loginfo(twist)
 			twist.angular.z = -0.25
 			self.pub.publish(twist)
-			rospy.sleep(7)
+			rospy.sleep(8)
 			twist.angular.z = 0
 		elif angle == 180:
 			twist.angular.z = 0.50
 			self.pub.publish(twist)
 			rospy.sleep(7)
+			twist.angular.z = 0
+		elif angle == 270:
+			twist.angular.z = 0.25
+			self.pub.publish(twist)
+			rospy.sleep(10.5)
+			#rospy.loginfo(twist)
+			twist.angular.z = -0.25
+			self.pub.publish(twist)
+			rospy.sleep(21)
 			twist.angular.z = 0
 		self.pub.publish(twist)
 		
